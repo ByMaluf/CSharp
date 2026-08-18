0d09bd6881fc8762f88fad5fe86405cdea4cb7ef
Brenno Ysrael M. L. Correia <brenno.maluf@gmail.com>
2026-08-18 19:15:13 -0400

    Reorganiza Conceitos e Teorias em ordem de estudo

--- Codigo/HelloWorld/HelloWorld/obj/Debug/net10.0/HelloWorld.AssemblyInfo.cs
+++ Codigo/HelloWorld/HelloWorld/obj/Debug/net10.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a80e05d060bbca6a1e063d1fa50c22303040e97")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+365f51e71eb04c45787ac9f50c195b1a93afe2fb")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
